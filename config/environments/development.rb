8da6fd40eee6478601ceaab45749a37aaf09c0ce
ryuseitakeuchi <104980045+ryuseitakeuchi@users.noreply.github.com>
2022-05-21 08:18:10 +0000

    Added preview of outgoing email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,15 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.action_mailer.raise_delivery_errors = false
+
+  host = 'example.com' # ここをコピペすると失敗します。自分の環境のホストに変えてください。
+  # クラウドIDEの場合は以下をお使いください
+  config.action_mailer.default_url_options = { host: host, protocol: 'https' }
+  # localhostで開発している場合は以下をお使いください
+  # config.action_mailer.default_url_options = { host: host, protocol: 'http' }
+
+
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
