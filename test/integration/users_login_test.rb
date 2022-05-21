1129177adf64a70d1d44500bbb86a23714f68178
ryuseitakeuchi <104980045+ryuseitakeuchi@users.noreply.github.com>
2022-05-21 02:24:57 +0000

    Rails tutorial 9.1.4

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -6,18 +6,6 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     @user = users(:michael)
   end
 
-  test "login with valid email/invalid password" do
-    get login_path
-    assert_template 'sessions/new'
-    post login_path, params: { session: { email:    @user.email,
-                                          password: "invalid" } }
-    assert_not is_logged_in?
-    assert_template 'sessions/new'
-    assert_not flash.empty?
-    get root_path
-    assert flash.empty?
-  end
-
   test "login with valid information followed by logout" do
     get login_path
     post login_path, params: { session: { email:    @user.email,
@@ -32,6 +20,8 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     delete logout_path
     assert_not is_logged_in?
     assert_redirected_to root_url
+    # 2番目のウィンドウでログアウトをクリックするユーザーをシミュレートする
+    delete logout_path
     follow_redirect!
     assert_select "a[href=?]", login_path
     assert_select "a[href=?]", logout_path,      count: 0
