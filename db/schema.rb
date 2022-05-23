b1558c888d1d300ad08d9d5d1472cef28d0fb675
ryuseitakeuchi <104980045+ryuseitakeuchi@users.noreply.github.com>
2022-05-23 02:09:03 +0000

    Setting a new password

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_22_002152) do
+ActiveRecord::Schema.define(version: 2022_05_23_015736) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2022_05_22_002152) do
     t.boolean "activated", default: false
     t.datetime "activated_at"
     t.string "reset_digest"
+    t.datetime "reset_sent_at"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
